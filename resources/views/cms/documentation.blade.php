b819bd1d8e08875b20d6beb0fe1eab0a19fc8921
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-05-20 12:33:20 +0700

    Documentation page favicon

--- resources/views/cms/documentation.blade.php
+++ resources/views/cms/documentation.blade.php
@@ -16,7 +16,7 @@
     <meta http-equiv="cleartype" content="on">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon"/>
     <link rel="stylesheet" href="{{ asset('assets/documentation/css/hightlightjs-dark.css') }}">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.8.0/highlight.min.js"></script>
