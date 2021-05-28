2582a8952d1709e24c4739faa1874f40a7685ebd
Agung Setyo Pribadi <agungsetyopribadi8@gmail.com>
2021-05-28 22:13:00 +0700

    Fix cors issue

--- config/cors.php
+++ config/cors.php
@@ -15,9 +15,9 @@ return [
     |
     */
 
-    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['/*', 'api/*', 'sanctum/csrf-cookie'],
 
-    'paths' => ['*'],
+    // 'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
